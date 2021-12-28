28517ff375669908aeecfcbf70fce40ec3b20f49
lucaskaspary <lucaskaspary@hotmail.com>
2021-12-28 12:03:08 -0300

    adds a modelview matrix

--- public/main.js
+++ public/main.js
@@ -9,16 +9,79 @@ if(!gl) {
 
 
 const vertexData = [
-    0, 1, 0,
-    1, -1, 0,
-    -1, -1, 0
+    //Front
+    0.5,0.5,0.5,
+    0.5,-.5,0.5,
+    -.5,0.5,0.5,
+    -.5,0.5,0.5,
+    0.5,-.5,0.5,
+    -.5,-.5,0.5,
+
+    //Left
+    -.5,0.5,0.5,
+    -.5,-.5,0.5,
+    -.5,0.5,-.5,
+    -.5,0.5,-.5,
+    -.5,-.5,0.5,
+    -.5,-.5,-.5,
+
+    //Back
+    -.5,0.5,-.5,
+    -.5,-.5,-.5,
+    0.5,0.5,-.5,
+    0.5,0.5,-.5,
+    -.5,-.5,-.5,
+    0.5,-.5,-.5,
+
+    //Right
+    0.5,0.5,-.5,
+    0.5,-.5,-.5,
+    0.5,0.5,0.5,
+    0.5,0.5,0.5,
+    0.5,-.5,0.5,
+    0.5,-.5,-.5,
+
+    //Top
+    0.5,0.5,0.5,
+    0.5,0.5,-.5,
+    -.5,0.5,0.5,
+    -.5,0.5,0.5,
+    0.5,0.5,-.5,
+    -.5,0.5,-.5,
+
+    //Bottom
+    0.5,-.5,0.5,
+    0.5,-.5,-.5,
+    -.5,-.5,0.5,
+    -.5,-.5,0.5,
+    0.5,-.5,-.5,
+    -.5,-.5,-.5,
 ];
 
-const colorData = [
-    1,0,0,
-    0,1,0,
-    0,0,1,
-];
+// const colorData = [
+//     1,0,0,
+//     0,1,0,
+//     0,0,1,
+// ];
+
+function randomColor() {
+    return [Math.random(), Math.random(), Math.random()];
+}
+
+// let colorData = [
+//     ...randomColor(),
+//     ...randomColor(),
+//     ...randomColor(),
+// ]
+
+let colorData = [];
+for(let face = 0; face < 6; face++) {
+    let faceColor = randomColor();
+    for(let vertex = 0; vertex < 6; vertex ++) {
+        colorData.push(...faceColor);
+    }
+}
+
 
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -39,9 +102,11 @@ gl.shaderSource(vertexShader, `
     attribute vec3 color;
     varying vec3 vColor;
     
+    uniform mat4 matrix;
+    
     void main() {  
         vColor = color;
-        gl_Position = vec4(position, 1);
+        gl_Position = matrix * vec4(position, 1);
     }
 `);
 gl.compileShader(vertexShader);
@@ -76,7 +141,46 @@ gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 
 gl.useProgram(program);
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+gl.enable(gl.DEPTH_TEST);
+
+const uniformLocations = {
+    matrix: gl.getUniformLocation(program, `matrix`),
+};
+
+
+
+const viewMatrix = mat4.create();
+const modelMatrix = mat4.create();
+const projectionMatrix = mat4.create();
+const modelViewMatrix = mat4.create();
+const modelViewProjectionMatrix = mat4.create();
+
+
+
+mat4.perspective(projectionMatrix,
+    75 * Math.PI/180, //vertical field-of-view
+    canvas.width/canvas.height, // aspect W/H
+    1e-4, //near cull distance
+    1e4 // far cull distance
+);
+mat4.translate(modelMatrix, modelMatrix, [-1.5, 0, -2]);
+
+mat4.translate(viewMatrix, viewMatrix, [-3,0,1]);
+mat4.invert(viewMatrix,viewMatrix);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
+    mat4.multiply(modelViewProjectionMatrix,projectionMatrix,modelViewMatrix);
+    gl.uniformMatrix4fv(uniformLocations.matrix, false, modelViewProjectionMatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
+}
+
+animate();
+
+
 
 
 
